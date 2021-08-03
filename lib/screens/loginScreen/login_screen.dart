5bb07d6f1c4d2b5c5af28c1b7e8f88a1f55189de
askabdulhaseeb <askabdulhaseeb@gmail.com>
2021-08-03 14:48:30 +0500

    Start New Project

---
+++ lib/screens/loginScreen/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+  static const String routeName = '/LoginScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
